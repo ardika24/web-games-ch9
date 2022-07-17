77615ac6b2f8864c39a98122a700219b575bf97f
ardika24 <ardhieka24@gmail.com>
2022-07-17 22:49:18 +0700

    Update Backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 const { User } = require("../models");
 const { Op } = require("sequelize");
+const { hashPassword } = require("../utils/passwordHandler");
 
 class UserController {
   static async getUsers(req, res, next) {
@@ -34,6 +35,107 @@ class UserController {
         return res.status(200).json({
           result: "succes",
           data,
+        });
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async getUserById(req, res, next) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      if (user) {
+        return res.status(200).json({
+          result: "Success",
+          data: user,
+        });
+      } else {
+        return res.status(400).json({
+          result: "Not Found",
+          message: `Player with ${id} not found`,
+        });
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async createUser(req, res, next) {
+    try {
+      const { email, username, password, bio, city, social_media_url } =
+        req.body;
+      if (!username || !email) {
+        return res.status(400).json({
+          result: "Failed",
+          message: "username or email cannot be empty",
+        });
+      }
+      if (!password) {
+        return res.status(400).json({
+          result: "Failed",
+          message: "password cannot be empty",
+        });
+      }
+      const newUser = {
+        username,
+        email,
+        password: await hasPassword(password),
+        bio,
+        city,
+        social_media_url,
+      };
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async updateUser(req, res, next) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json({
+          result: "Failed",
+          message: `User with ${id} not Found`,
+        });
+      }
+      const updatedUser = await User.update(req.body, {
+        where: { id: id },
+      });
+      if (updatedUser == 1) {
+        return res.status(200).json({
+          result: "Success",
+          message: `User with id: ${id} successfully updated`,
+        });
+      } else {
+        return res.status(500).json({
+          result: "Failed",
+          message: "Failed to update",
+        });
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async deleteUser(req, res, next) {
+    try {
+      const { id } = req.params;
+
+      const destroyed = await User.destroy({
+        where: { id: id },
+      });
+      if (destroyed == 1) {
+        res.status(200).json({
+          result: "Success",
+          message: `User with id: ${id}, was deleted successfully`,
+        });
+      } else {
+        res.status(400).json({
+          result: "Failed",
+          message: `Cannot delete User with id: ${id}`,
         });
       }
     } catch (err) {
--- server/routes/v1/user.router.js
+++ server/routes/v1/user.router.js
@@ -2,5 +2,9 @@ const userRouter = require("express").Router();
 const UserController = require("../../controllers/userController");
 
 userRouter.get("/", UserController.getUsers);
+userRouter.get("/:id", UserController.getUserById);
+userRouter.post("/", UserController.createUser);
+userRouter.put("/:id", UserController.updateUser);
+userRouter.delete("/:id", UserController.deleteUser);
 
 module.exports = userRouter;
