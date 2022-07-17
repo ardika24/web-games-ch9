3c40dd2e1bd942eb4d42ee66d36b2739ed5e5b4b
ardika24 <ardhieka24@gmail.com>
2022-07-17 13:16:53 +0700

    fix error test api get data

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const apiRouter = require("./routes/v1");
+const apiRouter = require("./routes");
 const PORT = process.env.PORT || 4000;
 
 const app = express();
