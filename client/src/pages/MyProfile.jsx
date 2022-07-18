0bdb2d6cdb62710700dfb7c53aa6f741eb1c5a2c
raditya-rizky <gusrizkydharmaputra123@gmail.com>
2022-07-18 20:06:09 +0800

    adding myprofile page

---
+++ client/src/pages/MyProfile.jsx
@@ -0,0 +1,67 @@
+import { Button } from "react-bootstrap";
+import style from "../pages/MyProfile.module.css";
+
+export default function MyProfile() {
+  function back() {
+    window.history.back();
+  }
+  return (
+    <div className="container-fluid">
+      <div className={style.back}>
+        <img
+          src="/back-button.png"
+          alt="back"
+          style={{ width: "3rem" }}
+          onClick={back}
+        />
+        <span>
+          <button onClick={back}>BACK</button>
+        </span>
+      </div>
+      <div className="row pt-5">
+        <div className="col-lg-5">
+          <div className="left text-light">
+            <p className="text-center">Here's your profile.</p>
+            <p className="text-center">
+              You can edit by clicking the "Edit" button.
+            </p>
+            <img
+              src="/cartoon.png"
+              alt="cartoon"
+              className="img-fluid"
+              style={{ width: "35em" }}
+            />
+          </div>
+        </div>
+        <div className="col-lg text-light">
+          <div className={style.right}>
+            <div className="container p-4">
+              <h2 className="text-center">YOUR PROFILE</h2>
+              <br />
+              <div className="info">
+                <h5>Email:</h5>
+                <p>email.com</p>
+                <h5>Username:</h5>
+                <p>username</p>
+                <h5>Total Score:</h5>
+                <p>1000</p>
+                <h5>Bio:</h5>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+                  quidem perferendis aliquid cumque blanditiis labore?
+                </p>
+                <h5>Twitter:</h5>
+                <p>@twitter</p>
+                <h5>City:</h5>
+                <p>zimbabwe</p>
+              </div>
+              <Button type="submit" className={style.loginButton}>
+                Edit
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
