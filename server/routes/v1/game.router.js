1cc1c4f8dc15fa9e9ceeacfeb181e732e740ce65
ardika24 <ardhieka24@gmail.com>
2022-08-06 07:56:33 +0700

    miss it

--- server/routes/v1/game.router.js
+++ server/routes/v1/game.router.js
@@ -1,12 +1,13 @@
 const gameRouter = require("express").Router();
 const GameController = require("../../controllers/gameController");
+const restrict = require("../../middlewares/restrict");
 
 gameRouter.get("/", (_, res) => {
   res.send("from game");
 });
 
 // @ /api/v1/games
-gameRouter.get("/high-score", GameController.getHighScore);
-gameRouter.put("/:id", GameController.updateScore);
+gameRouter.get("/high-score", restrict, GameController.getHighScore);
+gameRouter.put("/:id", restrict, GameController.updateScore);
 
 module.exports = gameRouter;
